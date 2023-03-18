35d4497bf8e61c313d33a7fd4806b945e330c951
Kauã Amaral <kauaamaral10@gmail.com>
2023-03-18 17:04:01 -0300

    Exercicio 4 finalizado

--- Sci.CSharp.Kaua.Amaral/QuatroCSharp.cs
+++ Sci.CSharp.Kaua.Amaral/QuatroCSharp.cs
@@ -5,6 +5,15 @@
         public void Execute()
         {
             //  4- Criar um vetor de cinco posições, solicitar  cinco números e ao fim, imprimir o valor apresentado na posição três.
+
+            double[] numbers = new double[5];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}° número: ");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine($"O valor apresentado na posição três é: {numbers[2]}");
         }
     }
 }
