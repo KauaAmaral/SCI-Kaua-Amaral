db890c3e8360c280211fca6712408686182d3603
Kauã Amaral <kauaamaral10@gmail.com>
2023-03-18 16:59:00 -0300

    Exercicio 3 finalizado

--- Sci.CSharp.Kaua.Amaral/TresCSharp.cs
+++ Sci.CSharp.Kaua.Amaral/TresCSharp.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.ConstrainedExecution;
-
-namespace Sci.CSharp.Kaua.Amaral
+﻿namespace Sci.CSharp.Kaua.Amaral
 {
     internal class TresCSharp
     {
@@ -11,6 +9,43 @@ namespace Sci.CSharp.Kaua.Amaral
             // todo este conjunto deverá estar contido em um laço que confirme se deseja encerrar o programa ou continuar solicitando outros nomes e notas.
             // Ao final da solicitação do nome e das notas deverá ser impresso o nome do aluno e a sua média,
             // se a nota for  menor que 6 imprimir Reprovado, senão, se a nota for igual ou maior que 6, imprimir Aprovado.
+
+            string name;
+            double note1, note2, note3, note4, average;
+            char option;
+
+            do
+            {
+                Console.Write("Nome do aluno: ");
+                name = Console.ReadLine();
+
+                Console.Write("1° nota: ");
+                note1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("2° nota: ");
+                note2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("3° nota: ");
+                note3 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("4° nota: ");
+                note4 = Convert.ToDouble(Console.ReadLine());
+
+                average = (note1 + note2 + note3 + note4) / 4;
+
+                Console.WriteLine(@$"Aluno: {name}
+Média: {average}");
+
+                if (average < 6)
+                    Console.WriteLine("Situação: Reprovadado");
+
+                if (average >= 6)
+                    Console.WriteLine("Situação: Aprovado");
+
+                Console.WriteLine("Deseja encerrar o programa? (S/N)");
+                option = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            } while (option != 'S');
         }
     }
 }
